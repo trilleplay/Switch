0f3ef1c3d3ffc4a266291bf4c187158e9e96986b
Trilleplay <trille@trilleplay.net>
2017-06-21 19:14:23 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'March 03 2017 00:00:00';
+var deadline = 'Juni 24 2017 00:00:00';
 initializeClock('clockdiv', deadline);
